fd50b7ece487538b0a54d9501bf1f3bb2b85bd8d
ricarsurbusiness <ricarsurbusiness@gmail.com>
2025-08-15 16:50:14 -0500

    feat: migration with news tables

---
+++ prisma/migrations/20250815214415_baseline_schema/migration.sql
@@ -0,0 +1,117 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('ADMIN', 'EMPLOYED');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "fullname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'EMPLOYED',
+    "num_cel" TEXT NOT NULL,
+    "id_type" TEXT NOT NULL,
+    "num_id" TEXT NOT NULL,
+    "branch_id" INTEGER,
+    "image" TEXT,
+    "state" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Branch" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "state" BOOLEAN NOT NULL DEFAULT true,
+    "num_tel" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Branch_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Product" (
+    "id" SERIAL NOT NULL,
+    "code" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "id_category" INTEGER,
+    "req_prescription" BOOLEAN NOT NULL DEFAULT false,
+    "id_laboratory" INTEGER,
+    "id_distributor" INTEGER,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Laboratory" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Laboratory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Distributor" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "num_tel" TEXT,
+    "email" TEXT,
+
+    CONSTRAINT "Distributor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Inventory_branch" (
+    "id" SERIAL NOT NULL,
+    "id_branch" INTEGER NOT NULL,
+    "id_product" INTEGER NOT NULL,
+    "stock_min" INTEGER NOT NULL,
+    "current_stock" INTEGER NOT NULL,
+    "location" TEXT NOT NULL,
+    "state" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Inventory_branch_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_code_key" ON "public"."Product"("code");
+
+-- AddForeignKey
+ALTER TABLE "public"."User" ADD CONSTRAINT "User_branch_id_fkey" FOREIGN KEY ("branch_id") REFERENCES "public"."Branch"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Product" ADD CONSTRAINT "Product_id_category_fkey" FOREIGN KEY ("id_category") REFERENCES "public"."Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Product" ADD CONSTRAINT "Product_id_laboratory_fkey" FOREIGN KEY ("id_laboratory") REFERENCES "public"."Laboratory"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Product" ADD CONSTRAINT "Product_id_distributor_fkey" FOREIGN KEY ("id_distributor") REFERENCES "public"."Distributor"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Inventory_branch" ADD CONSTRAINT "Inventory_branch_id_branch_fkey" FOREIGN KEY ("id_branch") REFERENCES "public"."Branch"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Inventory_branch" ADD CONSTRAINT "Inventory_branch_id_product_fkey" FOREIGN KEY ("id_product") REFERENCES "public"."Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
